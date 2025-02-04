af0ddd3b9af4756e871f07dbcb2d8f0894b028f5
Nathalie <nat933@free.fr>
2025-02-04 20:11:31 +0100

    first commit

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class RecipeController extends AbstractController
+{
+    #[Route('/recette', name: 'recipe.index')]
+    public function index(Request $request): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]+', 'id' => '\d+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        return $this->json([
+            'slug' => $slug,
+            'id' => $id
+        ]);
+    }
+}
