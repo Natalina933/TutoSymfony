13f0e76b1d3a06ff1bf7217929fe680515ec6576
Nathalie <nat933@free.fr>
2025-02-05 18:28:23 +0100

    maj

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -9,19 +9,23 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class RecipeController extends AbstractController
 {
-    #[Route('/recette', name: 'recipe.index')]
+    #[Route('/recettes', name: 'recipe.index')]
     public function index(Request $request): Response
     {
-        return $this->render('recipe/index.html.twig', [
-            'controller_name' => 'RecipeController',
-        ]);
+        return $this->render('recipe/index.html.twig');
     }
-    #[Route('/recette/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]+', 'id' => '\d+'])]
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]+', 'id' => '\d+'])]
     public function show(Request $request, string $slug, int $id): Response
     {
-        return $this->json([
+        return $this->render('recipe/show.html.twig', [
             'slug' => $slug,
-            'id' => $id
+            'id' => $id,
+            'name' => '<strong>Recette ' . $slug . '</strong>',
+            'person' => [
+                'firstname' => 'John',
+                'lastname' => 'Doe',
+
+            ]
         ]);
     }
 }
