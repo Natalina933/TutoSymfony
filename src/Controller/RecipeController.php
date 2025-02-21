5f992f365b43dce548b300ffaaa57bcd4e9a6ef0
Nathalie <nat933@free.fr>
2025-02-21 11:57:08 +0100

    Mise place mappage recette

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,14 +11,21 @@ use Symfony\Component\Routing\Attribute\Route;
 final class RecipeController extends AbstractController
 {
     #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request): Response
+    public function index(Request $request, RecipeRepository $repository): Response
     {
-        return $this->render('recipe/index.html.twig');
+        $recipes = $repository->findAll();
+        // dd($recipes);
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipes,
+        ]);
     }
     #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]+', 'id' => '\d+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    public function show(Request $request, string $slug, int $id, RecipeRepository $reposotory): Response
     {
+        $recipe = $reposotory->find($id);
+        // dd($recipe);
         return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
             'slug' => $slug,
             'id' => $id,
             'name' => '<strong>Recette ' . $slug . '</strong>',
