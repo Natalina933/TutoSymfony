3dadf4a20e2907a958dd16109d994e9075e2dd92
Nathalie <nat933@free.fr>
2025-02-26 16:23:33 +0100

    EntityManagerInterface

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -7,11 +7,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+use App\Entity\Recipe;
 
 final class RecipeController extends AbstractController
 {
+    // On utilise l'EntityManager pour persister et supprimer des données
+    // En utilisant Doctrine ORM, il est possible d'accéder à l'EntityManager via l'injecteur de dépendances $this->em
+    /**
+     * Affiche la liste des recettes qui prennent moins de 50 minutes à faire.
+     *
+     * @param Request $request
+     * @param RecipeRepository $repository
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
     #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request, RecipeRepository $repository): Response
+    public function index(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
     {
         $recipes = $repository->findAll();
         // dd($recipes);
@@ -19,6 +32,16 @@ final class RecipeController extends AbstractController
             'recipes' => $recipes,
         ]);
     }
+    /**
+     * Affiche une recette.
+     *
+     * Vérifie que le slug correspond à la recette demandée, sinon redirige vers l'URL correcte.
+     * Si la recette n'est pas trouvée, redirige vers la page d'accueil.
+     *
+     * @param string $slug Slug de la recette
+     * @param int $id Identifiant de la recette
+     * @return Response
+     */
     #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]+', 'id' => '\d+'])]
     public function show(Request $request, string $slug, int $id, RecipeRepository $reposotory): Response
     {
